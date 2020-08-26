fb6b86f4c43a8755b97c2a684ab59207710b55a7
linus106-bit <linus3390@gmail.com>
2020-08-26 15:49:21 +0900

    20208026

--- Red_detection.py
+++ Red_detection.py
@@ -2,24 +2,43 @@ import cv2
 import numpy as np
 import math
 
+
 cap = cv2.VideoCapture(0)
+
+class FindNonZero:
+    def AllNonZero(height,width,img):
+        count_img = 0
+        for k in range(3):
+            for i in range(height):
+                for j in range(width):
+                    if img[int(i),int(j),int(k)] != 0:
+                        for find in range (i,height):
+                            if img[int(find),int(j),int(k)] != 0:
+                                count_img += 1
+                            else:
+                                break
+                    else:
+                        continue
+        print(count_img)
 
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     #frame = cv2.flip(frame,0)
     #frame = cv2.flip(frame,1)
+    frame = cv2.resize(frame, dsize=(320, 240), interpolation=cv2.INTER_AREA)
     
     if(ret) :
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        height, width = frame.shape[:2] # 높이 너비
+        
         # 빨간색은 hsv에서 0-10, 170-180 두개로 검출해야됨
         lower_red = cv2.inRange(hsv, (0,100,100), (5,255,255))
         upper_red = cv2.inRange(hsv, (175,100,100), (180,255,255))
         added_red = cv2.addWeighted(lower_red, 1.0 ,upper_red, 1.0, 0.0) # 합친거
         red = cv2.bitwise_and(frame, frame, mask= added_red) 
-
-        non_zero = np.count_nonzero(red)
-        print(non_zero)
+        FindNonZero.AllNonZero(height,width,red)
+        a =1 
         cv2.imshow('red',red)
         k = cv2.waitKey(1) & 0xFF
         if k == 27 :
