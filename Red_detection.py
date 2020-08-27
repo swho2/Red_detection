294f1b5f2172c40a388fbb516db68d25d2f5112b
linus106-bit <linus3390@gmail.com>
2020-08-27 15:48:43 +0900

    20200827

--- Red_detection.py
+++ Red_detection.py
@@ -8,17 +8,16 @@ cap = cv2.VideoCapture(0)
 class FindNonZero:
     def AllNonZero(height,width,img):
         count_img = 0
-        for k in range(3):
-            for i in range(height):
-                for j in range(width):
-                    if img[int(i),int(j),int(k)] != 0:
-                        for find in range (i,height):
-                            if img[int(find),int(j),int(k)] != 0:
-                                count_img += 1
-                            else:
-                                break
-                    else:
-                        continue
+        for i in range(height):
+            for j in range(width):
+                if img[int(i),int(j)] > 0:
+                    for find in range (i,height):
+                        if img[int(find),int(j)] != 0:
+                            count_img += 1
+                        else:
+                            break
+                else:
+                    continue
         print(count_img)
 
 
@@ -36,10 +35,11 @@ while(cap.isOpened()):
         lower_red = cv2.inRange(hsv, (0,100,100), (5,255,255))
         upper_red = cv2.inRange(hsv, (175,100,100), (180,255,255))
         added_red = cv2.addWeighted(lower_red, 1.0 ,upper_red, 1.0, 0.0) # 합친거
-        red = cv2.bitwise_and(frame, frame, mask= added_red) 
-        FindNonZero.AllNonZero(height,width,red)
-        a =1 
+        red = cv2.bitwise_and(frame, frame, mask= added_red)
+        h,s,v = cv2.split(red)
+        FindNonZero.AllNonZero(height,width,v)
         cv2.imshow('red',red)
+        cv2.imshow("new_red",v)
         k = cv2.waitKey(1) & 0xFF
         if k == 27 :
             break
